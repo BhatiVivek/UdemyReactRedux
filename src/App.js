de88e1a71e31cd17aceb8902221582af3a7ee653
Vivek Bhati <vivekbhati9192@gmail.com>
2019-07-15 10:14:43 +0530

    full changes with onchange and delete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import { find, findIndex } from 'lodash';
 
 class App extends Component{
   state = {
-    person: [
-      {name: 'Gunjan', age:'27', profssion: 'I am a Govt Employee'},
-      {name: 'Vivek', age:'27', profssion: 'I am an Engineer'},
-      {name: 'Rajendra', age:'55', profssion: 'I am a Business man'},
-      {name: 'Krishna', age:'50', profssion: 'I am a beautisian'},
+    persons: [
+      { id: '1', name: 'Gunjan', age:'27', profssion: 'I am a Govt Employee'},
+      { id: '2', name: 'Vivek', age:'27', profssion: 'I am an Engineer'},
+      { id: '3', name: 'Rajendra', age:'55', profssion: 'I am a Business man'},
+      { id: '4', name: 'Krishna', age:'50', profssion: 'I am a beautisian'},
     ],
     otherState: "some other value",
-    isBlockVisible: false,
-    value: null,
+    showPersons: false,
   };
 
-  toggleDivHandler = () => {
-    this.setState({ isBlockVisible: !this.state.isBlockVisible });
-  }
-
-  onChage = (event) => {
-    this.setState({
-      person: [
-        {profssion: event.target.value},
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = findIndex(this.state.persons, (p) => {
+      return p.id === id;
     });
+
+    // way 1
+    // not a good way, because we are mutating the state here.
+    // const { persons } = this.state;
+    // persons[personIndex].name = event.target.value;
+    // this.setState({
+    //   persons
+    // });
+
+    // Way 2
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name =  event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons });
+
+    //Way 3 not working as of now
+    // this.setState(prevState => ({
+    //   persons: {
+    //     ...prevState.persons,
+    //     [prevState.persons[personIndex].name]: event.target.value,
+    //   },
+    // }));
   }
 
-  deletePersonHandler = (index) => {
-    console.log('asdfasdfasdf');
-    const person = this.state.person;
-    person.splice(index, 1);
-    this.setState({ person });
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons });
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render () {
@@ -39,26 +65,25 @@ class App extends Component{
       backgroundcolor: 'white',
       border: '1px solid blue',
       cursor: 'pointer',
-      margin: '16px auto',
-      height: '30px',
+      padding: '8px',
+      font: 'inherit',
     };
 
     let person = null;
 
-    if(this.state.isBlockVisible){
+    if(this.state.showPersons){
       person = (
         <div>
           {
-            this.state.person.map((person, index) => {
+            this.state.persons.map((person, index) => {
               return (
                 <Person
-                  key={index}
+                  key={person.id}
                   name={person.name}
                   age={person.age}
-                  change={() => this.onChage()}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
                   onClick={() => this.deletePersonHandler(index)}
-                >{person.profssion}
-              </Person>
+                />
               );
             })
           }
@@ -68,12 +93,14 @@ class App extends Component{
   
     return (
       <div className="App">
-        {person}
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working!</p>
         <button
             style={style}
-            onClick={this.toggleDivHandler}>
-            Click to show and hide Data
+            onClick={this.togglePersonHandler}>
+           Toggle Persons
           </button>
+          {person}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,14 +5,16 @@ import logo from '../logo.svg'
 const person = (props) => {
     return (
         <div className="Person">
-          <div className="container-fluid row">
-            <p className="col-sm-8">Hi my name is {props.name}</p>
-            <img src={logo}  onClick={props.onClick} className="imgCancle col-sm-4" />
-          </div>
-          <p>My age is {props.age}</p>
-          <span>Enter some text here</span>
-          <input type="text" value={props.value} onChange={props.change}/>
+          {/* <div className="container container-fluid row">
+            <p className="col-sm-11">Hi my name is {props.name}</p>
+            <i class="far fa-window-close"></i>
+            <img src={logo}  onClick={props.onClick} className="imgCancle col-sm-1" />
+          </div> */}
+          {/* <p>My age is {props.age}</p> */}
+          {/* <span>Enter some text here</span> */}
+          <p onClick={props.onClick}>I'm {props.name} and I am {props.age}</p>
           <p>{props.children}</p>
+          <input type="text" value={props.name} onChange={props.changed}/>
         </div>
     )
 };
